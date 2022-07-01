d9b0196c190a0c1ab57aef405e745154556363ce
user <stel-vd@yandex.ru>
2022-07-02 00:04:02 +0300

    add test case 10845

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,39 @@
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@Feature("Авторизация")
+@Owner("allure8")
+public class AuthTest {
+
+    @Test
+    @AllureId("10845")
+    @DisplayName("Авторизация через яндекс для Автоматизации")
+    @Tags({@Tag("blocker"), @Tag("web tests")})
+
+    public void testGoogleAuth () {
+        step("Открываем главную страницу");
+        step("Нажимаем кнопку Авторизация");
+        step("Выбираем способ авторизации через Яндекс");
+        step("Авторизуемся как пользователь 'Mrs.Random'", () -> {
+            step("Вводим логин 'random-user'");
+            step("Вводим пароль 'random-123' ");
+            step("Нажимаем кнопку Войти");
+        });
+        step("Должны оказаться на главной странице сайта");
+        step("Профиль пользователя должен быть заполнен из Яндекс", () -> {
+            step("Имя 'Mrs.Random'");
+            step("Логин 'random-user'");
+            step("Пароль '123'");
+            step("аватар");
+        });
+        step("");
+        step("Разлогиниваемся");
+    }
+}
